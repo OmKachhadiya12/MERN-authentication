b23bbe94d1824d1a9a5ac423fc7282fad74effed
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-09-25 16:27:14 +0530

    Adeed a react-router-dom.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import ResetPassword from "./pages/ResetPassword.jsx"
+import VerifyEmail from "./pages/VerifyEmail.jsx"
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/resetpassword',
+    element: <ResetPassword />
+  },
+  {
+    path: '/verifyemail',
+    element: <VerifyEmail />
+  },
+  {
+    path: '*', // 404 route
+    element: <div>Page Not Found</div>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
