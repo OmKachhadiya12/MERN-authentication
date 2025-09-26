4fd6d4173c770917ab31cd3e5a64cc2cf38af4ce
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-09-26 11:41:32 +0530

    adding the components.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flrx flex-col items-center mt-20px-4 text-center text-gray-800'>
+      <img src={assets.header_img} alt="" className='w-36 h-36 rounded-full mb-6' />
+      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey Developer <img src={assets.hand_wave} alt="" className='w-8 aspect-square'/></h1>
+      <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to your app.</h2>
+      <p className='mb-8 max-w-md'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit atque architecto tempora voluptates numquam sit quisquam libero consequatur natus! Quas, provident expedita accusamus distinctio numquam at assumenda et quisquam ratione iure qui sit, quod praesentium saepe in nihil cupiditate nulla vitae. Accusamus itaque debitis dolore quam deserunt nobis, totam et?</p>
+      <button className='border border-gray-500 rounded-ful px-8 py-2.5 hover:bg-gray-100  transition-all'>Get Started</button>
     </div>
   )
 }
