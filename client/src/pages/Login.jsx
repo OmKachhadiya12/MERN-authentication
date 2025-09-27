f94c737315339fba9dacc9e46b91870984d20dcd
om kachhadiya <kachhadiyaom2005@gmail.com>
2025-09-27 13:53:34 +0530

    Working on Login page.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,36 +1,63 @@
 import React, { useState } from 'react';
-import assets from "./../assets/assets.js";
+import {assets} from "./../assets/assets.js";
 
 const Login = () => {
-    const [state,setState] = useState("Sign Up")
+    const [state,setState] = useState("signup");
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
   return (
-    <div className='flrx items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
         <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
 
-        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm  '>
-          <h2 className='text-3xl font-semibold terxt-white text-center mb-3'>{state == "signup" ? "Create account." : "Login "}</h2>
+        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+          <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state == "signup" ? "Create account." : "Login "}</h2>
           <p className='text-center text-sm mb-6 '>{state == "signup" ? "Create your account." : "Login to your acoount."}</p>
 
           <form>
-              <div className='mb-4 felx items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            {state === 'signup' && (
+              <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                 <img src={assets.person_icon} alt="" />
-                <input className='bg-transparent outline-none' type="text" placeholder='Full name' required />
+                <input
+                 onChange={e => setName(e.target.value)}
+                 value = {name}
+                 className='bg-transparent outline-none' type="text" placeholder='Full name' required />
               </div>
+            )}
+              
 
-              <div className='mb-4 felx items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+              <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                 <img src={assets.mail_icon} alt="" />
-                <input className='bg-transparent outline-none' type="email" placeholder='Email id' required />
+                <input
+                  onChange={e => setEmail(e.target.value)}
+                  value={email} 
+                  className='bg-transparent outline-none' type="email" placeholder='Email id' required />
               </div>
 
-              <div className='mb-4 felx items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+              <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                 <img src={assets.lock_icon} alt="" />
-                <input className='bg-transparent outline-none' type="password" placeholder='Password' required />
+                <input 
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                  className='bg-transparent outline-none' type="password" placeholder='Password' required />
               </div>
 
               <p className='mb-4 text-indigo-500 cursor-pointer'>Forget password??</p>
 
               <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}</button>
-          </form>
+          </form> 
+
+          {state === 'signup' ? (
+            <p className='text-gray-400 text-center text-xs mt-4'>Already have an account??  {'  '}
+            <span onClick={() => setState('Login')} className='text-blue-400 cursor-pointer underline'>Login here</span>
+            </p>
+          ) : (
+            <p className='text-gray-400 text-center text-xs mt-4'>Don't have an account??  {'  '}
+            <span onClick={() => setState('signup')} className='text-blue-400 cursor-pointer underline'>Sign up</span>
+            </p>
+          )}
 
         </div>
     </div>
@@ -38,5 +65,3 @@ const Login = () => {
 }
 
 export default Login
-
-
